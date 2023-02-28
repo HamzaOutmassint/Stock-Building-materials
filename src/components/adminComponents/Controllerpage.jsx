55f2a992f830978b98ae71c62145aacb8119d307
ELFATIMI SALAHEDDINE <salahfatimi16@gmail.com>
2023-02-28 14:30:48 +0100

    init

--- src/components/adminComponents/Controllerpage.jsx
+++ src/components/adminComponents/Controllerpage.jsx
@@ -16,35 +16,28 @@ export default function Controllerpage() {
           <div className="flex  sm:justify-end gap-6">
             <div>
               <input
-                className="bg-[#3C3D42]  px-10 h-9 w-full 'bg-[#4B484C] rounded-xl focus:outline none p-1 text-white text-xs"
+                className="bg-[#3C3D42]  px-8 h-9 w-full 'bg-[#4B484C] rounded-xl focus:outline none p-1 text-white text-xs"
                 type="text"
                 onChange={(e) => setSearchname(e.target.value)}
                 placeholder="Search for ... "
               />
             </div>
             <div className="flex gap-4 items-center">
-              
               <select
                 name=""
                 id=""
-              
                 onChange={(e) => setSearchdate(e.target.value)}
-                className="bg-[#3C3D42]  px-10 h-9 w-full 'bg-[#4B484C] rounded-xl focus:outline none p-1 text-white text-xs"
+                className="bg-[#3C3D42]  px-8 h-9 w-full 'bg-[#4B484C] rounded-xl focus:outline none p-1 text-white text-xs"
               >
-                <option selected >
-                  All date
-                </option>
-                <option  value="1">
-                  1
-                </option>
+                <option selected>All date</option>
+                <option value="1">1</option>
               </select>
             </div>
           </div>
         </section>
         <div className=" ">
-        <AdminTable/>
+          <AdminTable />
         </div>
-       
       </div>
     </div>
   );

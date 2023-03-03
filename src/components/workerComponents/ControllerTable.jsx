794b66d73fc2a0d3306df9b675c0c2df66c87d6e
ELFATIMI SALAHEDDINE <salahfatimi16@gmail.com>
2023-03-03 15:25:41 +0100

    afficher tablue par sprecilaty

--- src/components/workerComponents/ControllerTable.jsx
+++ src/components/workerComponents/ControllerTable.jsx
@@ -9,49 +9,46 @@ import Paper from '@mui/material/Paper';
 import axios from "axios";
 import {useState , useEffect} from "react";
 
-function createData(name, calories, fat, carbs) {
-  return { name, calories, fat, carbs };
+function createData(name,Quantity_Completed, The_remaining_quantity, Number_of_Persons) {
+  return { name,Quantity_Completed, The_remaining_quantity, Number_of_Persons };
 }
 
 // style for inputs
 const InputStyle = 'bg-[#4B484C] rounded-sm focus:outline none p-1 text-white text-xs'
 
-
 export default function BasicTable({speciality}) {
-  const [designatio , setDesignatio] = useState([])
-
+  const [designation , setDesignation] = useState([])
 
   useEffect(()=>{
     axios.post("http://localhost/project_atlass/getDesignation.php",{speciality:speciality}).then((res)=>{
-     
-
-      setDesignatio(res.data)
+      setDesignation(res.data.designationName)
     }).catch((err)=>{
       console.log(err)
     })
   })
 
-  
+  const affi = designation?.map((ele)=>(
+   createData(ele.designationName,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>)
 
-  const rows = [
-    // createData('hamza',<input type="number" className={InputStyle} />,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
-    // createData('hicham',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
-    // createData('youssef',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
-    // createData('norddin',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
-    // createData('salah',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
-    createData('walid',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
-    // createData('brahim',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
-    createData('brahim',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
-    // createData('brahim',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
+  ))
+
+
+  // const rows = [
+  //   // createData('hamza',<input type="number" className={InputStyle} />,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
+  //   // createData('hicham',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
+  //   // createData('youssef',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
+  //   // createData('norddin',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
+  //   // createData('salah',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
+  //   createData('walid',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
+  //   // createData('brahim',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
+  //   createData('brahim',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
+  //   // createData('brahim',<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>,<input type="number" className={InputStyle}/>),
   
     
   
-  ];
+  // ];
 
-  // const affi = designatio.map((ele,index)=>(
-  //   console.log(ele.designationName)
-  // ))
-  console.log(designatio)
+  
 
 
   return (
@@ -62,16 +59,16 @@ export default function BasicTable({speciality}) {
             <TableCell style={{"color":"#fff" , "fontSize":"20px" , "fontWeight":600 , "paddingLeft":"30px"}}>Designation</TableCell>
             <TableCell align="center" style={{"color":"#fff" , "fontSize":"20px" , "fontWeight":600}}>Quantity_Completed</TableCell>
             <TableCell align="center" style={{"color":"#fff" , "fontSize":"20px" , "fontWeight":600}}>The_remaining_quantity</TableCell>
-            <TableCell align="right" style={{"color":"#fff" , "fontSize":"20px" , "fontWeight":600 ,  "paddingRight":"30px"}}>Number_of_Persons</TableCell>
+            <TableCell align="center" style={{"color":"#fff" , "fontSize":"20px" , "fontWeight":600 ,  "paddingRight":"30px"}}>Number_of_Persons</TableCell>
           </TableRow>
         </TableHead>
         <TableBody className="bg-[#1F2025]">
-          {rows.map((row) => (
+          {affi?.map((row) => (
             <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
               <TableCell component="th" scope="row" style={{"color":"#fff","paddingLeft":"30px"}}>{row.name}</TableCell>
-              <TableCell align="center">{row.calories}</TableCell>
-              <TableCell align="center">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="center">{row.Quantity_Completed}</TableCell>
+              <TableCell align="center">{row.The_remaining_quantity}</TableCell>
+              <TableCell align="center">{row.Number_of_Persons}</TableCell>
             </TableRow>
           ))}
         </TableBody>

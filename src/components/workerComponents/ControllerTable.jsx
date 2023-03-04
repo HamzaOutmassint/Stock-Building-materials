9ae09dfaa216349d59bbd9123dfcc39dc1c88093
HamzaOutmassint <outmassinthamza@gmail.com>
2023-03-04 16:46:26 +0000

    not important

--- src/components/workerComponents/ControllerTable.jsx
+++ src/components/workerComponents/ControllerTable.jsx
@@ -17,17 +17,11 @@ function createData(name,Quantity_Completed, The_remaining_quantity, Number_of_P
 // style for inputs
 const InputStyle = 'bg-[#4B484C] rounded-sm focus:outline none p-1 text-white text-xs text-center'
 
-export default function BasicTable({speciality,date,idControler,blocname } ) {
-  // state have designation
+export default function BasicTable({speciality , pushItem}) {
   const [designation , setDesignation] = useState([])
-
-  console.log(blocname)
-  //arraye have donne of input par desenation
   const [items , setItems] = useState([])
-  
-  // obejet par input of designation
-  const [eachItem , setEachItem] = useState({blocName:blocname,idControler:idControler,dateValidation:date,designation:"",Quantity_Completed:0,The_remaining_quantity:0,Number_of_Persons:0})
-  // get desenation from php
+  const [eachItem , setEachItem] = useState({designation:"",Quantity_Completed:"",The_remaining_quantity:"",Number_of_Persons:""})
+
   useEffect(()=>{
     if(speciality !== ""){
       axios.post("http://localhost/project_atlass/getDesignation.php",{speciality:speciality}).then((res)=>{
@@ -38,87 +32,38 @@ export default function BasicTable({speciality,date,idControler,blocname } ) {
     }
   },[speciality])
 
-  //test
-  const arraydes=designation?.map((ele)=>({blocName:blocname,idControler:idControler,dateValidation:date,designation:ele.designationName,Quantity_Completed:0,The_remaining_quantity:0,Number_of_Persons:0}))
-  
-  console.log(arraydes)
-
-// on change input function
   const handlChange=(e)=>{
-
-    // get name designation
     const designationName = e.target.id
-
-    //serach if designation is found in array
-    const serach = items.map((ele)=>{
-      if(ele.designation==designationName){
-        return true
-      }})
-
-
-    // insert value in first time
     if(eachItem.designation==""){
       setEachItem((prev)=>(
-        {...prev , [e.target.name] : e.target.value ,designation:designationName,blocName:blocname}
+        {...prev , [e.target.name] : e.target.value ,designation:designationName}
       ))
-    //insert value in meme obejet
     }else if (designationName==eachItem.designation){
-    /// if not existe in  array 
-    if (serach==false){
-      
       setEachItem((prev)=>(
-        {...prev , [e.target.name] : e.target.value ,designation:designationName,blocName:blocname}
-      
-        ))}else{
-
-          //find old date
-          const olddonne=items.find((ele)=>ele.designation ==designationName)
-          
-          //save old date to update 
-          if(olddonne!=undefined){
-          setEachItem({idControler:idControler,dateValidation:date,designation:"",Quantity_Completed:olddonne.Quantity_Completed,The_remaining_quantity:olddonne.The_remaining_quantity,Number_of_Persons:olddonne.Number_of_Persons,blocName:blocname})
-          }
-         
-         
-          
-       //updtaing array 
-        setItems( items.filter((ele)=>
-         ele.designation !==designationName
-           ))
-
-       // whene have same desitination we need too insert new objet of the same designation
-           setEachItem((prev)=>(
-            {...prev , [e.target.name] : e.target.value ,designation:designationName,blocName:blocname}
-          ))
-       
-      }
-   //insert objet in items and get empty setEachItem
-    }else if(designationName!=eachItem.designation  ){
-
-      //appned in array items
+        {...prev , [e.target.name] : e.target.value ,designation:designationName}
+      ))
+    }else if(designationName !== eachItem.designation){
       setItems(items => [...items,eachItem] );
-
-      //empty setEachItem 
-      setEachItem({idControler:idControler,dateValidation:date,designation:"",Quantity_Completed:0,The_remaining_quantity:0,Number_of_Persons:0,blocName:blocname})
+      setEachItem({designation:"",Quantity_Completed:"",The_remaining_quantity:"",Number_of_Persons:""})
       setEachItem((prev)=>(
-        {...prev , [e.target.name] : e.target.value ,designation:designationName,blocName:blocname}
+        {...prev , [e.target.name] : e.target.value ,designation:designationName}
       ))
     }
-    
 }
-  
-console.log(items)
-  
-// map of designation and create input for each designation
+  console.log(eachItem)
+  console.log(items)
+  const valid=(designationName)=>{
+    console.log(eachItem)
+  }
+
   const afficherDonner = designation?.map((ele,key)=>(
    createData(
     ele.designationName,
-    <input type="number" id={ele.designationName} defaultValue={0} name='Quantity_Completed' onChange={(e)=>handlChange(e)}  className={InputStyle}/>,
-    <input type="number" id={ele.designationName} defaultValue={0} name='The_remaining_quantity' onChange={(e)=>handlChange(e)} className={InputStyle}/>,
-    <input type="number" id={ele.designationName} defaultValue={0} name='Number_of_Persons' onChange={(e)=>handlChange(e)} className={InputStyle}/>
+    <input type="number" id={ele.designationName} defaultValue="0" name='Quantity_Completed' onChange={(e)=>handlChange(e)}  className={InputStyle}/>,
+    <input type="number" id={ele.designationName} defaultValue="0" name='The_remaining_quantity' onChange={(e)=>handlChange(e)} className={InputStyle}/>,
+    <input type="number" id={ele.designationName} defaultValue="0" name='Number_of_Persons' onChange={(e)=>handlChange(e)} className={InputStyle}/>
    )
   ))
-
 
   if(designation.length !== 0){
     return(
@@ -130,6 +75,7 @@ console.log(items)
               <TableCell align="center" style={{"color":"#fff" , "fontSize":"20px" , "fontWeight":600}}>Quantity_Completed</TableCell>
               <TableCell align="center" style={{"color":"#fff" , "fontSize":"20px" , "fontWeight":600}}>The_remaining_quantity</TableCell>
               <TableCell align="center" style={{"color":"#fff" , "fontSize":"20px" , "fontWeight":600 ,  "paddingRight":"30px"}}>Number_of_Persons</TableCell>
+              <TableCell align="center" style={{"color":"#fff" , "fontSize":"20px" , "fontWeight":600 ,  "paddingRight":"30px"}}>Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody className="bg-[#1F2025]">
@@ -139,7 +85,12 @@ console.log(items)
                 <TableCell align="center">{row.Quantity_Completed}</TableCell>
                 <TableCell align="center">{row.The_remaining_quantity}</TableCell>
                 <TableCell align="center">{row.Number_of_Persons}</TableCell>
-                
+                <TableCell align="center">
+                  <Button style={{"backgroundColor":"#BBE1FA", "color":"#1b1919" ,"textTransform":"lowercase","fontWeight":600,"width": "100px"}}
+                   >
+                    valid
+                  </Button>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>

54e8802989668f61a155a64698ea2fad7b9bc3d8
ELFATIMI SALAHEDDINE <salahfatimi16@gmail.com>
2023-03-08 17:04:59 +0100

    fix some probleme

--- src/components/adminComponents/OptionsPage.jsx
+++ src/components/adminComponents/OptionsPage.jsx
@@ -7,7 +7,7 @@ export default function OptionsPage() {
   return (
     <div className="px-4  container mx-auto">
       <div className="grid  grid-cols-1 md:grid-cols-2 py-1 md:h-screen items-center gap-20">
-        <NavLink to={"../AllControlers"} onClick={(e) => setChose(e.currentTarget.id)} id="Controller" className=" bg-[#1F2025] hover:bg-[#313239] transition duration-300 space-y-10 py-28   rounded-lg shadow-2xl ">
+        <NavLink to={"../AllControlers"}   className=" bg-[#1F2025] hover:bg-[#313239] transition duration-300 space-y-10 py-28   rounded-lg shadow-2xl ">
           <div className="w-52 m-auto">
             <img src={Controler} alt="" />
           </div>
@@ -15,7 +15,7 @@ export default function OptionsPage() {
             <span>Controller</span>
           </div>
         </NavLink>
-        <NavLink to={"../Blocpage"} onClick={(e) => setChose(e.currentTarget.id)} id="Apartment" className=" bg-[#1F2025] hover:bg-[#313239] transition duration-300 space-y-10 py-28 rounded-lg shadow-2xl ">
+        <NavLink to={"../Blocpage"}   className=" bg-[#1F2025] hover:bg-[#313239] transition duration-300 space-y-10 py-28 rounded-lg shadow-2xl ">
           <div className="w-52 m-auto">
             <img src={building} alt="" />
           </div>
